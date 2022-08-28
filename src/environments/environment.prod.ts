302be7aab17488d5b77b493c45216c2feea63f4c
benrouba <31619294+benrouba@users.noreply.github.com>
2022-08-28 12:32:27 +0100

    Update environment.prod.ts
    updatse

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   twitterUrl:"https://api.twitter.com/1.1/statuses/home_timeline.json?count=100&tweet_mode=extended",
   twitterCredentials:{
-   oauth_consumer_key:"FhTomOQh9ebul76elsbfDu7s0", 
-   oauth_token:"910852669211803648-dtZWrfMphU57wPbfZXUEDhaa3mWXcdt", 
-   oauth_signature_method:"HMAC-SHA1", 
-   oauth_timestamp:"1646730973", 
-   oauth_nonce:"Tyh4G9", 
+   oauth_consumer_key:"", 
+   oauth_token:"", 
+   oauth_signature_method:"", 
+   oauth_timestamp:"", 
+   oauth_nonce:"", 
    oauth_version:"1.0", 
-   oauth_signature:"LPXEY1X2hxRbYv7%2FzzH1%2FXulkOw%3D",
-   "Access-Control-Allow-Origin": 'http://localhost:4200'
+   oauth_signature:"",
+   "Access-Control-Allow-Origin": ''
   }
 };
